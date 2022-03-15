5efa84e67671c79143b7d7b3b323c16be7fa87ca
WinterSoldier16 <vsg8@hi.is>
2022-03-15 17:31:24 +0000

    lagaði schema og insert

--- sql/insert.sql
+++ sql/insert.sql
@@ -10,9 +10,9 @@ INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('OSTBORGAR
 INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('BEIKONBORGARI', 1319, 'með osti, salati, beikoni og hamborgarasósu', 'https://res.cloudinary.com/hbv2-h1/image/upload/v1647362914/vef2-h1/baconburger_nqeds7.jpg', 'Hamborgarar');
 INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('BBQ-BORGARI', 1259, 'með osti, salati, svissuðum lauk og BBQ-sósu', 'https://res.cloudinary.com/hbv2-h1/image/upload/v1647362920/vef2-h1/bbqburger_v8tvbk.jpg', 'Hamborgarar');
 
-INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('RISTUÐ SAMLOKA', 949, 'með skinku, osti og hamborgarasósu', 'https://res.cloudinary.com/hbv2-h1/image/upload/v1647363757/vef2-h1/GrilledSandwich_m2fvns.jpg', 'Samloku');
-INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('B.L.T. SAMLOKA', 1329, 'með skinku, osti, beikoni, salati, tómötum og sinnepssósu', 'https://res.cloudinary.com/hbv2-h1/image/upload/v1647363749/vef2-h1/BLTSandwich_eujczj.jpg', 'Samloku');
-INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('BAGUETTE', 839, 'með skinku, osti og sósu að eigin vali', 'https://res.cloudinary.com/hbv2-h1/image/upload/v1647363745/vef2-h1/baguette_z5glca.jpg', 'Samloku');
+INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('RISTUÐ SAMLOKA', 949, 'með skinku, osti og hamborgarasósu', 'https://res.cloudinary.com/hbv2-h1/image/upload/v1647363757/vef2-h1/GrilledSandwich_m2fvns.jpg', 'Samlokur');
+INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('B.L.T. SAMLOKA', 1329, 'með skinku, osti, beikoni, salati, tómötum og sinnepssósu', 'https://res.cloudinary.com/hbv2-h1/image/upload/v1647363749/vef2-h1/BLTSandwich_eujczj.jpg', 'Samlokur');
+INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('BAGUETTE', 839, 'með skinku, osti og sósu að eigin vali', 'https://res.cloudinary.com/hbv2-h1/image/upload/v1647363745/vef2-h1/baguette_z5glca.jpg', 'Samlokur');
 
 INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('LAUKHRINGIR', 1019, 'Djúpsteiktir og brakandi', 'https://res.cloudinary.com/hbv2-h1/image/upload/v1647363757/vef2-h1/onionrings_epp3sl.jpg', 'Aðrir réttir');
 INSERT INTO vorur (title, price, description, image, flokkar) VALUES ('KJÚKLINGANAGGAR', 1049, '6stk af ljúffengum nöggum', 'https://res.cloudinary.com/hbv2-h1/image/upload/v1647363754/vef2-h1/ChickenNuggets_vmtfb0.jpg', 'Aðrir réttir');
